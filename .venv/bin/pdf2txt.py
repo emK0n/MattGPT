69c8c8da1314da6f1fae7e7f82b42729461d038e
emK0n <matt.konwiser@gmail.com>
2025-08-12 07:13:06 -0400

    commit

--- .venv/bin/pdf2txt.py
+++ .venv/bin/pdf2txt.py
@@ -1,4 +1,4 @@
-#!/Users/mattk/MattGPT/.venv/bin/python3.12
+#!/Users/mattk/Documents/GitHub/MattGPT/.venv/bin/python3.13
 """A command line tool for extracting text and images from PDF and
 output it to plain text, html, xml or tags.
 """
